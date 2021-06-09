e4dad17836bd690396c3781780fd7a2ee970e8a3
Oliver <oliver@wecashup.com>
2021-06-09 09:02:33 +0200

    Add new Animation rotate

--- app/src/main/java/android/kotlin/pixelmoving/activity/MainActivity.java
+++ app/src/main/java/android/kotlin/pixelmoving/activity/MainActivity.java
@@ -3,23 +3,25 @@ package android.kotlin.pixelmoving.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.kotlin.pixelmoving.R;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
-import java.util.logging.SimpleFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
     TextView text_view_square;
     TextView text_view_coordinate;
 
-    private  int xDelta = 0;
-    private int yDelta = 0;
+    private int xMove = 0;
+    private int yMove = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         text_view_coordinate = findViewById(R.id.text_view_coordinate);
         moveTextview();
 
-
     }
 
 
@@ -51,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
     private void moveTextview(){
 
             relative_layout_square.setOnTouchListener(onTouchListener());
-
     }
 
     private View.OnTouchListener onTouchListener() {
@@ -71,11 +71,12 @@ public class MainActivity extends AppCompatActivity {
                         RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
                                 view.getLayoutParams();
 
-                        xDelta = x - lParams.leftMargin;
-                        yDelta = y - lParams.topMargin;
+                        xMove = x - lParams.leftMargin;
+                        yMove = y - lParams.topMargin;
                         Animation animation = new RotateAnimation(0.0f,180.0f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
                         animation.setDuration(200);
                         animation.setFillEnabled(true);
+                        animation.setRepeatCount(Animation.INFINITE);
                         animation.setFillAfter(true);
                         text_view_square.startAnimation(animation);
                         break;
@@ -87,16 +88,16 @@ public class MainActivity extends AppCompatActivity {
                         @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleFormatter = new SimpleDateFormat("dd/MM:mm:ss");
                         String dateText = simpleFormatter.format(date);
                         text_view_coordinate.setText(String.format("(%s:%s) |%s", xText, yText, dateText));
+                        text_view_square.clearAnimation();
                         break;
 
                     case MotionEvent.ACTION_MOVE:
                         RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
                                 .getLayoutParams();
-                        layoutParams.leftMargin = x - xDelta;
-                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.leftMargin = x - xMove;
+                        layoutParams.topMargin = y - yMove;
                         layoutParams.rightMargin = 0;
                         layoutParams.bottomMargin = 0;
-
                         view.setLayoutParams(layoutParams);
                         break;
                 }
